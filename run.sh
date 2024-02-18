5acb70b7f693ea353d9669cc28de99212418c18b
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-02-18 19:13:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,14 @@
 # https://github.com/oneclickvirt/lxc_arm_images
 # 2024.02.09
 
+
+urls=($(curl -s https://jenkins.linuxcontainers.org/view/Images/job/image-openwrt/api/xml | grep -oP '<url>.*?architecture=arm64.*?</url>' | grep -oP 'https://.*?(?=<\/url>)'))
+arm64_urls=()
+for url in "${urls[@]}"; do
+    if [[ $url == *'architecture=arm64'* ]]; then
+        arm64_urls+=("$url")
+    fi
+done
+for url in "${arm64_urls[@]}"; do
+    echo "$url"
+done
