bca855c8f2c536f6e507afb5e225c078ff77854d
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-03-06 17:06:01 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,33 +2,37 @@
 # by https://github.com/oneclickvirt/lxc_arm_images
 # 2024.03.06
 # curl -L https://raw.githubusercontent.com/oneclickvirt/lxc_arm_images/main/test.sh -o test.sh && chmod +x test.sh && ./test.sh
+#!/bin/bash
 
-rm -rf log
-rm -rf fixed_images.txt
+log_file="log"
+fixed_images_file="fixed_images.txt"
 date=$(date)
-echo "$date" >>log
-echo "------------------------------------------" >>log
+echo "$date" >> "$log_file"
+echo "------------------------------------------" >> "$log_file"
 release_names=("ubuntu" "debian" "kali" "centos" "almalinux" "rockylinux" "fedora" "opensuse" "alpine" "archlinux" "gentoo" "openwrt" "oracle" "openeuler")
 system_names=()
 response=$(curl -slk -m 6 "https://raw.githubusercontent.com/oneclickvirt/lxc_arm_images/main/all_images.txt")
 if [ $? -eq 0 ] && [ -n "$response" ]; then
     system_names+=($(echo "$response"))
 fi
-for ((i = 0; i < ${#release_names[@]}; i++)); do
-    release_name="${release_names[i]}"
+for release_name in "${release_names[@]}"; do
     temp_images=()
     for sy in "${system_names[@]}"; do
         if [[ $sy == "${release_name}"* ]]; then
-            curl -m 60 -LO "https://github.com/oneclickvirt/lxc_arm_images/releases/download/${release_name}/${sy}"
-            if [ $? -ne 0 ]; then
-                curl -m 60 -LO "https://cdn.spiritlhl.net/https://github.com/oneclickvirt/lxc_arm_images/releases/download/${release_name}/${sy}"
+            # Check if the file exists
+            if curl --output /dev/null --silent --head --fail "https://github.com/oneclickvirt/lxc_arm_images/releases/download/${release_name}/${sy}"; then
+                temp_images+=("$sy")
+            else
+                if curl --output /dev/null --silent --head --fail "https://cdn.spiritlhl.net/https://github.com/oneclickvirt/lxc_arm_images/releases/download/${release_name}/${sy}"; then
+                    temp_images+=("$sy")
+                fi
             fi
-            temp_images+=("${sy}")
         fi
     done
     for image_name in "${temp_images[@]}"; do
         echo "$image_name"
-        echo "$image_name" >>log
-        echo "$image_name" >>fixed_images.txt
+        echo "$image_name" >> "$log_file"
+        echo "$image_name" >> "$fixed_images_file"
+        echo "------------------------------------------" >> "$log_file"
     done
 done
